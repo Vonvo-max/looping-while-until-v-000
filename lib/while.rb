041501650ec466b7711cd56881aea7a752916fc7
Max Ringelheim <max.ringelheim@gmail.com>
2020-01-31 14:54:36 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -4,6 +4,7 @@ def using_while
     levitation_force += 1
     puts "Wingardium Leviosa"
 end
-end
 
-using_while 
+using_while  #this is calling the method so that i can run the code in the terminal
+             # you also have to remember to map your file and folder in the terminal 
+           #when you run the code so that ruby knows how to find it
