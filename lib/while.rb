0ba84c4bf9cd433c5c9b8c1c98735a927444afbf
Max Ringelheim <max.ringelheim@gmail.com>
2020-01-31 13:57:33 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,6 +1,6 @@
-def using_while
+def using_while(levitation_force)
   levitation_force = 6
-	while using_while < 10
+	while levitation_force < 10
     puts "Wingardium Leviosa"
     counter += 1
 end
