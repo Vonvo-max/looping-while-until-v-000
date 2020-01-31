ce9f6aea769cec87ebe876da0ccf71288a6b3131
Max Ringelheim <max.ringelheim@gmail.com>
2020-01-31 14:16:29 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,7 @@
-def using_while(looping_string)
+def using_while(levitation_force)
   levitation_force = 6
 	while levitation_force < 10
-    puts "Wingardium Leviosa"
     looping_string += 1
+    puts "Wingardium Leviosa"
 end
 end
